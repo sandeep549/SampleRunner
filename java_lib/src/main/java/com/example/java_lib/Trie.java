d64d4d9e4338425238400b386b12b7a76fe61686
Sandeep K <sandeepkumar.dev@gmail.com>
2019-02-24 09:44:48 +0530

    Add search suggestions

--- java_lib/src/main/java/com/example/java_lib/Trie.java
+++ java_lib/src/main/java/com/example/java_lib/Trie.java
@@ -35,6 +35,43 @@ public class Trie {
         return node != null && node.isEndOfWord;
     }
 
+    /**
+     * This function will print all the words in the dictionary
+     * that have given string as prefix.
+     * 1. Check if prefix itself not present.
+     * 2. Check if prefix end itself as word.
+     * 3. Print all words that has this prefix.
+     */
+    public void printSuggestions(String prefix) {
+        TrieNode node = root;
+        for (int i = 0; i < prefix.length(); ++i) {
+            int index = prefix.charAt(i) - 'a';
+            if (node.children[index] == null)
+                return; // prefix not present
+            node = node.children[index];
+        }
+        if (node != null && node.isEndOfWord) {
+            System.out.println(prefix);
+            return; // prefix itself is word
+        }
+        printSuggestions(prefix, node);
+    }
+
+    private void printSuggestions(String prefix, TrieNode node) {
+        if (node == null) {
+            return;
+        }
+        if (node.isEndOfWord)
+            System.out.println(prefix);
+        for (int i = 0; i < node.children.length; i++) {
+            char ch = (char) ('a' + i);
+            printSuggestions(prefix + ch, node.children[i]);
+        }
+    }
+
+    /**
+     * Node in Trie data structure.
+     */
     private static class TrieNode {
         private TrieNode[] children = new TrieNode[SIZE];
         private boolean isEndOfWord = false;
